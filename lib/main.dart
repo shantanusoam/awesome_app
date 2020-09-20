8ba9bc355c28d894e200b317846b60b35ac95476
shantanu <bluestackssoam@gmail.com>
2020-09-20 10:36:21 +0530

    ch3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    title: "Awesome App",
+    title: "Shrishtis App",
     home: HomePage(),
   ));
 }
@@ -14,28 +14,44 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(11),
-            boxShadow: [BoxShadow(color: Colors.grey[400],blurRadius: 6, offset: Offset(2.0,5.0))],
-            color: Colors.pinkAccent,
-            gradient: LinearGradient(
-                colors: [Colors.yellow, Colors.pink])
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
           ),
-          child: Text("Iam Sristhi",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 15,
-          ),),
         ),
       ),
-
     );
   }
 }
