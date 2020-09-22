4d2ee26ffbe8e6a84440e47a241d0cabf4105957
shantanu <bluestackssoam@gmail.com>
2020-09-22 08:20:38 +0530

    chapter5 Completed

--- lib/bgimge.dart
+++ lib/bgimge.dart
@@ -6,6 +6,8 @@ class BgImage extends StatelessWidget {
     return Image.asset(
       "assets/bg.jpg",
       fit: BoxFit.cover,
+      color: Colors.black.withOpacity(0.7),
+      colorBlendMode: BlendMode.darken,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:awesome_app/pages/home_page.dart';
+import 'package:awesome_app/pages/loginPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Shrishtis App",
-    home: HomePage(),
+    home: LoginPage(),
     theme: ThemeData(
       primarySwatch: Colors.purple,
     ),
+    routes: {
+      "/login": (context) => LoginPage(),
+      "/home": (context) => HomePage(),
+    },
   ));
 }
